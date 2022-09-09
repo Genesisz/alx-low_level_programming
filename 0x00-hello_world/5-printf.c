b8b97ea2744bebbb2554f4d8523ef62e369daa3b
Genesisz <genesiscostantine@gmail.com>
2022-09-09 09:22:11 -0700

    preprocessor

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
- * 
- * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle
- * Return: 0
- */
+* main - print string x option
+*
+* this program prints whatever printf summates
+* Return: D
+*/
 int main(void)
 {
-       printf("with proper grammar, but the outcome is a piece of art,\n");
-       return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
