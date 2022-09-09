0b45af39d31eae7d3cb0c9cc93dec9c0b25bfcd7
Genesisz <genesiscostantine@gmail.com>
2022-09-08 19:13:02 -0700

    preprocessor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
-include <studio.h>
+#include <stdio.h>
+
 /**
 * main - print the string in the put function
-*
 * Description: using the main function
-* this program prints -Programming is like building a multilingual puzzle
+* this program prints "Programming is like building a multilingual puzzle
 * Return: D
 */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle);
-
+puts("\"Programming is like building a multilingual puzzle);
 return (0);
-
+}
