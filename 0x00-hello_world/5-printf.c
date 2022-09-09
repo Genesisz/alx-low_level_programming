ccfdd146933026d8f027bc2015153172febcf011
Genesisz <genesiscostantine@gmail.com>
2022-09-08 20:11:25 -0700

    preprocessor

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,22 +1,13 @@
+
 #include <stdio.h>
 
 /**
- * my main is - entry point
+ * main - Entry point
  *
- *Return: Always 0 
+ * return: Always 0 (Success)
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-
-	printf("Size of a char: %d byte(s)\n", sizeof(a));
-	printf("Size of an int: %d byte(s)\n", sizeof(b));
-	printf("Size of a long int: %d byte(s)\n", sizeof(c));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
-	printf("Size of a float: %d byte(s)\n", sizeof(e));
+	printf("with proper grammar, but the outcome of art\n");
 	return (0);
 }
