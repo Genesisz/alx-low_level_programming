33418f5dfdd8e7f4792951a74827f94d3ce67d16
Genesisz <genesiscostantine@gmail.com>
2022-09-10 15:59:30 -0700

    preprocessor

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -24,12 +24,13 @@ int main(void)
 	}
 	else
 		if (n < 0)
+		
 		{
 			printf("%d is zero\n", n);
 		}
 		else
 		{
-			printf("%d is zero\n",n);
+			printf("%d is zero\n", n);
 		}
 	return (0);
 }
