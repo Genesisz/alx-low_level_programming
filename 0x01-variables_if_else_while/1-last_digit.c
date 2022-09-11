4c122680d8b533ad56046143548fc31b94bca7bf
Genesisz <genesiscostantine@gmail.com>
2022-09-10 19:10:41 -0700

    preprocessor

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,9 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
+/**
  * main - prints the last digit of the random
  * number stored in the variable n
+ * Description: using the main function
  * Return: Always 0 (Success)
  */
 int main(void)
