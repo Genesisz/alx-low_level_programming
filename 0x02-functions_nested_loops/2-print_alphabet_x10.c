0c4d31f88ea836e9e5cbda86ec0a28aec4e2e10a
Genesisz <genesiscostantine@gmail.com>
2022-09-13 18:38:44 -0700

    preprocessor

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function to print alphabex x10
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (1 = 0, i < = 9, 1++)
+	{
+		for (c = 'a', c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
