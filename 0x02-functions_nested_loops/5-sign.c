84e473594e4c872140bb9d4e1b7e9f51f01d997a
Genesisz <genesiscostantine@gmail.com>
2022-09-13 19:41:46 -0700

    preprocessor

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - function to check signs
+ * @n: is the int that will use for the argument of the function
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+}
+	else if (n < 0)
+	{
+	_putchar('-');
+	}
+	else
+	{
+	_putchar('0');
+	}
+}
