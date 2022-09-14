e21673c37b598812b22a6a7b0d91e94fc02b25a6
Genesisz <genesiscostantine@gmail.com>
2022-09-14 13:12:51 -0700

    preprocessor

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print_to_38 - Prints all natural numbers from input to 98
+ * print_to_98 - Prints all natural numbers from input to 98
  * @n: The number to begin counting at
  */
-would print_to_98(int n)
+void print_to_98(int n);
 {
 	if (n >= 98)
 	{
@@ -11,10 +11,11 @@ would print_to_98(int n)
 			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
+
 	else
 	{
 		while (n > 98)
 			printf("%d, ", n++);
-		printf("%d, n);
+		printf("%d\n", n);
 	}
 }
