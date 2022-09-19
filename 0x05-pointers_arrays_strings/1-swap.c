303a20e578dfedf7a017aa1ab47b552b174dff5d
Genesisz <genesiscostantine@gmail.com>
2022-09-19 15:37:20 -0700

    newcode

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the length of a string
+ *@s: eveluate strings
+ *Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
