c241379096fdd8b98a1da0246a66a439bc03240c
Genesisz <genesiscostantine@gmail.com>
2022-09-28 16:03:51 -0700

    powa

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
-
-
 /**
- *_puts_recursion - prints a string, followed by a new line
+ * _puts_recursion - prints a string, followed by a new line
  * @s: string to print
  */
-void _puts_recursion(char *j)
+void _puts_recursion(char *s)
 {
-	if (*j == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	_putchar(*j);
-	_puts_recursion(j + 1);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
