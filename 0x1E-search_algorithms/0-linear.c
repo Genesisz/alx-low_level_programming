e6ff798e6f7d2cfcf420c65605c1cf9c16f4786e
Genesisz <genesiscostantine@gmail.com>
2023-04-12 07:37:38 -0700

    network cc-executable

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,4 @@
-\#include "search_algos.h"
+#include "search_algos.h"
 
 /**
  * linear_search - searches for a value in an array of
@@ -24,3 +24,4 @@ int linear_search(int *array, size_t size, int value)
 	}
 	return (-1);
 }
+
